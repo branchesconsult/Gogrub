131b02ecddcafec23ab02c1d6da59a76e49d43a1
umair321 <P@werfull1>
2018-09-24 20:16:34 +0500

    order details

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -170,7 +170,7 @@ class OrderController extends Controller
         }])
             ->whereIn('id', $productIds)
             ->get();
-        dd($chef->toArray(), $chef->count());
+        dd($chef->count());
         return ($chef->count() > 1) ? false : $chef[0]->chef;
     }
 
