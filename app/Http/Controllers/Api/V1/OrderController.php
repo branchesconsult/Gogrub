92fcdfd0bbe63abebcd21f6ed29fb958ca3a1ee3
umair321 <P@werfull1>
2018-10-02 15:49:52 +0500

    merge

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -55,7 +55,7 @@ class OrderController extends Controller
      */
     public function store(MakeOrderRequest $request)
     {
-        dd($request->json()->all(), $request->all());
+        dd($request->json()->all(), $request->all(), $request->products);
         $products = $request->products;
         $customerPhone = $request->customer_phone;
         $customerAddress = $request->customer_address;
