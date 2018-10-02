7eaab4ce505505de55fa559890601e0c99725366
umair321 <P@werfull1>
2018-10-02 19:01:45 +0500

    que listnig

--- app/Console/Commands/EnsureQueueListenerIsRunning.php
+++ app/Console/Commands/EnsureQueueListenerIsRunning.php
@@ -91,7 +91,7 @@ class EnsureQueueListenerIsRunning extends Command
     private function startQueueListener()
     {
         //$command = 'php-cli ' . base_path() . '/artisan queue:listen --timeout=60 --sleep=5 --tries=3 > /dev/null & echo $!'; // 5.1
-        $command = 'php-cli ' . base_path() . '/artisan queue:work --timeout=60 --sleep=5 --tries=3 > /dev/null & echo $!'; // 5.6 - see comments
+        $command = 'php ' . base_path() . '/artisan queue:work --timeout=60 --sleep=5 --tries=3 > /dev/null & echo $!'; // 5.6 - see comments
         $pid = exec($command);
         return $pid;
     }
