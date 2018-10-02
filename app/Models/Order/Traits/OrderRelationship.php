c941fb50e4bfab1d72c21db27dcfb79dd2965406
umair321 <P@werfull1>
2018-10-02 15:51:45 +0500

    merge

--- app/Models/Order/Traits/OrderRelationship.php
+++ app/Models/Order/Traits/OrderRelationship.php
@@ -5,6 +5,7 @@ namespace App\Models\Order\Traits;
 use App\Models\Access\User\User;
 use App\Models\OrderDetails\OrderDetail;
 use App\Models\Orderstatus\Orderstatus;
+use App\Models\Product\Product;
 
 /**
  * Class OrderRelationship
@@ -33,5 +34,10 @@ trait OrderRelationship
     public function status()
     {
         return $this->belongsTo(Orderstatus::class, 'orderstatus_id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
     }
 }
