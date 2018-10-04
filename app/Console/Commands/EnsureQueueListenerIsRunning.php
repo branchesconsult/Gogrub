43a7be1f037790fd71c0a0bbedf2bf1a2aea09a1
umair321 <P@werfull1>
2018-10-04 14:08:51 +0500

    musc

--- app/Console/Commands/EnsureQueueListenerIsRunning.php
+++ app/Console/Commands/EnsureQueueListenerIsRunning.php
@@ -92,7 +92,9 @@ class EnsureQueueListenerIsRunning extends Command
     {
         //$command = 'php-cli ' . base_path() . '/artisan queue:listen --timeout=60 --sleep=5 --tries=3 > /dev/null & echo $!'; // 5.1
         $command = 'php ' . base_path() . '/artisan queue:work --timeout=60 --sleep=5 --tries=3 > /dev/null & echo $!'; // 5.6 - see comments
+        $command2 = 'laravel-echo-server start';
         $pid = exec($command);
-        return $pid;
+        $pid2 = exec($command2);
+        return $pid . ' ' . $pid2;
     }
 }
