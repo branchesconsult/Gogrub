4f34698f2404cd1848530a0fa5ea43be96169a5c
umair321 <P@werfull1>
2018-10-04 14:49:51 +0500

    echo server in que

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\EnsureLaravelEchoServerIsRunning;
 use App\Console\Commands\EnsureQueueListenerIsRunning;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -14,7 +15,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        EnsureQueueListenerIsRunning::class
+        EnsureQueueListenerIsRunning::class,
+        EnsureLaravelEchoServerIsRunning::class
     ];
 
     /**
