7255a4151877c797ebc11c101ceee2230958dd8d
umair321 <P@werfull1>
2018-10-09 18:59:49 +0500

    post ago in prder

--- app/Listeners/Frontend/Order/OrderCreateListener.php
+++ app/Listeners/Frontend/Order/OrderCreateListener.php
@@ -29,6 +29,7 @@ class OrderCreateListener implements ShouldQueue
      */
     public function handle(OrderCreateEvent $event)
     {
+        \Log::debug(print_r($event));
         $this->createNotification($event);
         $chefDeviceToken = Device::where('user_id', $event->order->chef_id)->get(['fcm_token']);
         $customerDeviceToken = Device::where('user_id', $event->order->customer_id)->get(['fcm_token']);
