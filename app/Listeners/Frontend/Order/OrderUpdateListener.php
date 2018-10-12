30f44de958032780489f80efb001d13ecad03c69
umair321 <P@werfull1>
2018-10-12 18:39:40 +0500

    mergng order

--- app/Listeners/Frontend/Order/OrderUpdateListener.php
+++ app/Listeners/Frontend/Order/OrderUpdateListener.php
@@ -3,6 +3,8 @@
 namespace App\Listeners\Frontend\Order;
 
 use App\Events\Frontend\Order\OrderUpdateEvent;
+use App\Models\Device\Device;
+use App\Models\Notification\Notification;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
@@ -26,6 +28,24 @@ class OrderUpdateListener
      */
     public function handle(OrderUpdateEvent $event)
     {
-        //
+        $this->createNotification($event);
+        $chefDeviceToken = Device::where('user_id', $event->order->chef_id)->get(['fcm_token']);
+        $customerDeviceToken = Device::where('user_id', $event->order->customer_id)->get(['fcm_token']);
+        sendPushNotificationToFCMSever($chefDeviceToken, 'You have updated order');
+        sendPushNotificationToFCMSever($customerDeviceToken, 'Your order has been updated.');
+        return false;
+    }
+
+
+    public function createNotification($event)
+    {
+        createNotification(
+            'Order has been created',
+            $event->order->customer_id,
+            Notification::ORDER_UPDATE,
+            $event->order->id,
+            [],
+            $event->order->chef_id
+        );
     }
 }
