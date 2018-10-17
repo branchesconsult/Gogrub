32ad6a10bdf083960d781a064dca50bdb46f5ea8
umair321 <P@werfull1>
2018-10-17 23:57:18 +0500

    hasRated

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -34,7 +34,7 @@ class FrontendController extends Controller
         })
             ->withCount('ratingReviews')
             ->get()->toArray();
-        //dd($data['products']);
+        dd($data['products']);
         return view('frontend.index', $data);
     }
 
