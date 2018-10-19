a72694693fd2de56d4cd701ec4fd5e34b800f667
umair321 <P@werfull1>
2018-10-19 20:18:07 +0500

    event lisetenr added

---
+++ app/Listeners/Frontend/Chat/SendChatEventListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners\Frontend\Chat;
+
+use App\Events\Frontend\Chat\SendChatEvent;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendChatEventListener implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(SendChatEvent $event)
+    {
+        \Log::info(print_r($event, true));
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        //
+    }
+}
