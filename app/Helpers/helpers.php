918687dc23eb007f6b9d8128087ac27d04a26708
umair321 <P@werfull1>
2018-10-19 12:00:02 +0500

    mergi

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -468,8 +468,9 @@ function getChefWithinDistance($lat, $lng, $searchWithIn = null)
  */
 function getChefDistanceFromUserLocation($chefId, $latLng = array())
 {
+
     //Distance in km
-    if ((empty($latLng) || !is_array($latLng)) || (!isset($latLng[0]) || !isset($latLng[0]))) {
+    if (count($latLng) != 2) {
         return 0;
     }
     $distanceSql = "SELECT ST_X(location_point) AS lat, 
